7cc4d595f827508b18a05178c7c3c91f46148ee4
wesleyk-duke <wesley.kendall@duke.edu>
2019-02-05 13:43:42 -0500

    further pep8 compliances

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -2,7 +2,8 @@ import pytest
 
 
 @pytest.mark.parametrize('test_input,expected',
-                         [('tachycardic', True), ('AtachycardicA', True), ('TACHYCARDIC', True), ('TaChYcArDiC', True),
+                         [('tachycardic', True), ('AtachycardicA', True),
+                          ('TACHYCARDIC', True), ('TaChYcArDiC', True),
                           ('sponge', False)])
 def test_is_tachycardic(test_input, expected):
     from tachycardia import is_tachycardic
