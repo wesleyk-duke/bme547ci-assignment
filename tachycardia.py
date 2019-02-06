0567e872bd580f0d2465388487acfcb0c31d7901
wesleyk-duke <wesley.kendall@duke.edu>
2019-02-06 11:02:03 -0500

    attempted to add some spell check

--- tachycardia.py
+++ tachycardia.py
@@ -1,6 +1,8 @@
 def is_tachycardic(arg_string):
     if 'tachycardic' in arg_string.lower():
         return True
+    elif 'tchycardic' or 'tahycardic' or 'tacycardic' or 'tachyardic' or 'tachycrdic' or 'tachycadic' or 'tachycard1c' or 'tachycardc' in arg_string.lower():
+        return True # yeahhh this is not how it should be done but i think you would need import an external library to spell check
     else:
         return False
 
