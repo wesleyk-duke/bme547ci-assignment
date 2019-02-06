11b06e6f9eadacaa9506a5ecd3299b00a18a9f54
wesleyk-duke <wesley.kendall@duke.edu>
2019-02-06 11:31:39 -0500

    attempt #3 at pep8 compliance

--- tachycardia.py
+++ tachycardia.py
@@ -1,14 +1,15 @@
 def is_tachycardic(arg_string):
     if 'tachycardic' in arg_string.lower():
         return True
-    elif 'tchycardic' in arg_string.lower() or 'tahycardic' in arg_string.lower()\
+    elif 'tchycardic' in arg_string.lower() \
+            or 'tahycardic' in arg_string.lower()\
             or 'tacycardic' in arg_string.lower()\
             or 'tachyardic' in arg_string.lower() \
             or 'tachycrdic' in arg_string.lower() \
             or 'tachycadic' in arg_string.lower() \
             or 'tachycard1c' in arg_string.lower() \
             or 'tachycardc' in arg_string.lower():
-        return True  
+        return True
     else:
         return False
 
